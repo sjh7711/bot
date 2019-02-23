b89dabbf69368d00532c6b803692ec9989750f64
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-02-23 15:07:15 +0900

    Update response.js

--- response.js
+++ response.js
@@ -427,7 +427,7 @@ function sel(r){ //flag[2]==0&&flag[3]==0 -> 초기값 // flag[2]==1&&flag[3]==0
 	}
 	
 	if(this["flag" + r.room][2]==1 && this["flag" + r.room][3]==0){
-		r.replier.reply("참여할 사람은 !참가 를 입력해주세요. 추첨을 제안한 사람이 !마감을 입력하면 마감됩니다.');
+		r.replier.reply("참여할 사람은 !참가 를 입력해주세요. 추첨을 제안한 사람이 !마감을 입력하면 마감됩니다.");
 		this["flag" + r.room][3]=1;
 	}
 	
