ba2c321b487320e9dd629346f7bce38f1586352b
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-02-24 14:16:25 +0900

    Update response.js

--- response.js
+++ response.js
@@ -357,20 +357,22 @@ function checkstatus(r){
 function overWatch(r) {
     var name = r.msg.substr(6).replace("#", "-");;//배틀태그가 담기는 공간
     var source = org.jsoup.Jsoup.connect('https://playoverwatch.com/ko-kr/career/pc/'+name).header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36").get();
-    var temp = source.select('div.masthead');
-    if (temp.select('div.u-align-center') == "" ) {
+    if (source.select('div.u-align-center').text().indexOf('이 프로필은 비공개입니다.')>0 ) {
     	r.replier.reply(name + "의 정보를 알 수 없습니다.");
 	} else {
+		var temp = source.select('div.masthead');
 		var comp = source.select('div.progress-category.toggle-display.is-active').get(1);
 		var quick = source.select('div.progress-category.toggle-display.is-active').get(0);
 		
 		var score = temp.select('div.u-align-center').get(0).text();
         var tier = temp.select('div.competitive-rank').get(0).toString().split('rank-icons/rank-')[1].split('Tier')[0];
+        
         var most1 = comp.select('div.ProgressBar-title').get(0).text();
         var most2 = comp.select('div.ProgressBar-title').get(1).text();
         var most3 = comp.select('div.ProgressBar-title').get(2).text();
         var most3 = comp.select('div.ProgressBar-title').get(3).text();
         var most3 = comp.select('div.ProgressBar-title').get(4).text();
+        
         r.replier.reply("점수 : "+score+"\n티어 : "+tier+"\n모스트 영웅\n1."+most1+"\n2."+most2+"\n3."+most3);
     }
 }
