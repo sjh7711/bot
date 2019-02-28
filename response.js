dbd5efcc5b4bfc8e4645988362de4bd140e69dde
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-02-28 20:28:57 +0900

    Update response.js

--- response.js
+++ response.js
@@ -329,11 +329,12 @@ function weather(r){
 		var link1 = ""
 		var link2 = 'https://m.weather.naver.com/m/main.nhn?regionCode=03220111';
 		var check = link2.indexOf('weather');
-          
-          if(input.length > 0){
-        	  I.register("weatherSelect",r.room,r.sender,function(input){
-        		  link1 = org.jsoup.Jsoup.connect("https://m.search.naver.com/search.naver?query="+input+"+날씨").get();
-		          link2 = link1.select('div.api_more_wrap').select('a').attr("abs:href");
+         
+		/*
+        if(input.length > 0){
+        	I.register("weatherSelect",r.room,r.sender,function(input){
+        		link1 = org.jsoup.Jsoup.connect("https://m.search.naver.com/search.naver?query="+input+"+날씨").get();
+        		link2 = link1.select('div.api_more_wrap').select('a').attr("abs:href");
 		          check = link2.indexOf('weather');
 		          if (check == -1){
 		        	  var temp = org.jsoup.Jsoup.connect("https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q="+input).get().select('div.cont_info').toArray();
@@ -366,65 +367,65 @@ function weather(r){
 			        	  return;
 			          }
 		          }
-        	  })
-          }
+      	  	 })
+        }*/
           
-          if(check > 0){
-        	  var doc = org.jsoup.Jsoup.connect(link2).get();
+        if(check > 0){
+        	var doc = org.jsoup.Jsoup.connect(link2).get();
 	          
-	          var data = doc.select('div._cnWtrHourlyChartData');
+        	var data = doc.select('div._cnWtrHourlyChartData');
 	          
-	          var clock = doc.select('span.th_text').text().split(' 내일')[0].split(' ').slice().concat('0시','3시','6시','9시','12시','15시','18시','21시','0시','3시','6시','9시','12시','15시','18시','21시','24시');
-	          var clock1 = doc.select('span.th_text').text().split(' 내일')[0].split(' ').slice().length;
+            var clock = doc.select('span.th_text').text().split(' 내일')[0].split(' ').slice().concat('0시','3시','6시','9시','12시','15시','18시','21시','0시','3시','6시','9시','12시','15시','18시','21시','24시');
+            var clock1 = doc.select('span.th_text').text().split(' 내일')[0].split(' ').slice().length;
 	          
-	          var sky = doc.select('tr.row.row_icon._cnWtrHourlyChart[data-tab=0]').text().split(' ').slice();
-	          var degree = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=0]').text().split(',').slice();
+	        var sky = doc.select('tr.row.row_icon._cnWtrHourlyChart[data-tab=0]').text().split(' ').slice();
+	        var degree = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=0]').text().split(',').slice();
 	          
-	          var rain = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=1]').text().split(',').slice();
+	        var rain = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=1]').text().split(',').slice();
 	          
-	          var wind = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=2]').text().split(',').slice();
-	          var direction = doc.select('tr.row.row_icon._cnWtrHourlyChart[data-tab=2]').text().split(' ').slice();
+	        var wind = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=2]').text().split(',').slice();
+	        var direction = doc.select('tr.row.row_icon._cnWtrHourlyChart[data-tab=2]').text().split(' ').slice();
 	          
-	          var wet = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=3]').text().split(',').slice();
-	          
-	          
+	        var wet = doc.select('div._cnWtrHourlyChartData').select('div[data-tab=3]').text().split(',').slice();
 	          
 	          
-	          var uv1 = doc.select('li.uv').select('em').text();
-	          var uv = doc.select('li.uv').select('span').text().replace(uv1, " ("+uv1+")");
 	          
-	          var index = doc.select('strong.title').text().replace('최근 검색한 곳','').split(' ').map(v=>String(v).replace(/온도/g, "온도 : ").replace(/지수/g, "지수 : "))
 	          
-	          var sun1 = doc.select('li.sun_item').select('div.day').select('span').get(0).text() +" : "+ doc.select('li.sun_item').select('div.time').get(0).text();
-	          var sun2 = doc.select('li.sun_item').select('div.day').select('span').get(1).text() +" : "+ doc.select('li.sun_item').select('div.time').get(1).text();
+	        var uv1 = doc.select('li.uv').select('em').text();
+	        var uv = doc.select('li.uv').select('span').text().replace(uv1, " ("+uv1+")");
+	         
+	        var index = doc.select('strong.title').text().replace('최근 검색한 곳','').split(' ').map(v=>String(v).replace(/온도/g, "온도 : ").replace(/지수/g, "지수 : "))
 	          
-	          var link3 = link2+'&default=air';
+	        var sun1 = doc.select('li.sun_item').select('div.day').select('span').get(0).text() +" : "+ doc.select('li.sun_item').select('div.time').get(0).text();
+	        var sun2 = doc.select('li.sun_item').select('div.day').select('span').get(1).text() +" : "+ doc.select('li.sun_item').select('div.time').get(1).text();
 	          
-	          var doc1 = org.jsoup.Jsoup.connect(link3).get();
-	          var pollution = doc1.select('li.pollution_item').toArray().map(v=>{vv=String(v.select('span.number').select('em').text()); vvv=String(v.select('span.title').text()); return vvv +" : "+ v.select('span.number').text().replace(vv, " "+vv)});
+	        var link3 = link2+'&default=air';
+	          
+	        var doc1 = org.jsoup.Jsoup.connect(link3).get();
+	        var pollution = doc1.select('li.pollution_item').toArray().map(v=>{vv=String(v.select('span.number').select('em').text()); vvv=String(v.select('span.title').text()); return vvv +" : "+ v.select('span.number').text().replace(vv, " "+vv)});
 
-	          var dust = doc1.select('div.dust_graph_number').toArray().map(v=>v.text().replace('먼지', '먼지 :')+"㎍/㎥");
+	        var dust = doc1.select('div.dust_graph_number').toArray().map(v=>v.text().replace('먼지', '먼지 :')+"㎍/㎥");
 	          
-	          var res = "";
-	          res += "시간 기온 강수% 습도           바람            날씨\n";
-	          for (var i = 0 ; i < clock1+8 ; i++) {
-	        	  res += clock[i].extension("0",3)+" ";
-                  res += String(degree[i]).extension(" ",3)+"℃ ";
-                  res += String(rain[i]).extension(" ",3)+"% ";
-                  res += String(wet[i]).extension(" ", 3)+"% ";
-                  res += String(wind[i]).extension(" ",3)+"m/s ";
-                  res += String(direction[i]).extension("   ",4)+" ";
-                  res += String(sky[i]).extension("   ",5)+" \n";
-	          }
-	          res += es;
-	          res += "------------기타지수------------\n";
-	          res += dust.join("\n");
-	          res += pollution.join("\n")+"\n";
-	          res += "\n자외선 : "+uv+"\n\n";
-	          res += "------------일상지수------------\n"+index.join("\n")+"\n";
-	          res += "\n------------일출&일몰-----------\n"+sun1+"\n"+sun2;
+	        var res = "";
+	        res += "시간 기온 강수% 습도           바람            날씨\n";
+	        for (var i = 0 ; i < clock1+8 ; i++) {
+	        	res += clock[i].extension("0",3)+" ";
+	        	res += String(degree[i]).extension(" ",3)+"℃ ";
+	        	res += String(rain[i]).extension(" ",3)+"% ";
+	        	res += String(wet[i]).extension(" ", 3)+"% ";
+	        	res += String(wind[i]).extension(" ",3)+"m/s ";
+	        	res += String(direction[i]).extension("   ",4)+" ";
+	        	res += String(sky[i]).extension("   ",5)+" \n";
+	        }
+	        res += es;
+	        res += "------------기타지수------------\n";
+	        res += dust.join("\n");
+	        res += pollution.join("\n")+"\n";
+	        res += "\n자외선 : "+uv+"\n\n";
+	        res += "------------일상지수------------\n"+index.join("\n")+"\n";
+	        res += "\n------------일출&일몰-----------\n"+sun1+"\n"+sun2;
 	    }
-    }
+	}
 	catch(e){
 		Api.replyRoom('test',e+"\n"+e.stack);
 	}
