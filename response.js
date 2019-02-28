67febf4ad8e79f99075840b2573dafc1a7a58bda
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-02-28 20:51:34 +0900

    Update response.js

--- response.js
+++ response.js
@@ -388,15 +388,15 @@ function weather(r){
 	        var dust = doc1.select('div.dust_graph_number').toArray().map(v=>v.text().replace('먼지', '먼지 :')+"㎍/㎥");
 	        var res = where+"\n";
 	        res += ""
-	        res += "시간 기온 강수% 습도           바람            날씨\n";
+	        res += "시간 기온 강수% 습도        바람         날씨\n";
 	        for (var i = 0 ; i < clock1+8 ; i++) {
 	        	res += clock[i].extension("0",3)+" ";
 	        	res += String(degree[i]).extension(" ",3)+"℃ ";
 	        	res += String(rain[i]).extension(" ",3)+"% ";
 	        	res += String(wet[i]).extension(" ", 3)+"% ";
 	        	res += String(wind[i]).extension(" ",3)+"m/s ";
-	        	res += String(direction[i]).extension("   ",4)+" ";
-	        	res += String(sky[i]).extension("   ",5)+" \n";
+	        	res += String(direction[i]).extension("   ",3)+" ";
+	        	res += String(sky[i]).extension("   ",4)+" \n";
 	        	if(i==4){
 	        		res += es;
 	        	}
