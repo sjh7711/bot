cbb4cbeda9d6255406cf63754d49198af2b799af
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-01 00:32:56 +0900

    Update response.js

--- response.js
+++ response.js
@@ -414,7 +414,7 @@ function weather(r){
 				res += "자외선 : "+uv+"\n";
 				res += "------------일상지수------------\n"+index.join("\n");
 				res += "\n------------일출&일몰-----------\n"+sun1+"\n"+sun2;
-				res += link2;
+				res += "\n"+link2;
 	        
 				r.replier.reply(res);
 			}
