a4cd2d83584b71ce4f0524639eb0892471391a1a
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-05 01:15:32 +0900

    Update response.js

--- response.js
+++ response.js
@@ -542,12 +542,12 @@ function weather(r){
 T.register("weatherClockCheck",()=>{
 	while(true){
 		if( 8 == new Date().getHours() ){
-			/*r={msg : '!날씨', room : 'agent',replier:{reply:function(msg){
+			r={msg : '!날씨', room : 'agent',replier:{reply:function(msg){
 				Api.replyRoom(r.room,msg)
 				}}
 			}
 			weather(r);
-			java.lang.Thread.sleep(6*1000);*/
+			java.lang.Thread.sleep(6*1000);
 			r={msg : '!날씨', room : 'bot',replier:{reply:function(msg){
 				Api.replyRoom(r.room,msg)
 				}}
@@ -566,11 +566,11 @@ T.register("weatherClockCheck",()=>{
 			}
 			weather(r);
 			java.lang.Thread.sleep(6*1000);
-			/*r={msg : '!날씨', room : 'ja',replier:{reply:function(msg){
+			r={msg : '!날씨', room : 'ja',replier:{reply:function(msg){
 				Api.replyRoom(r.room,msg)
 				}}
 			}
-			weather(r);*/
+			weather(r);
 			java.lang.Thread.sleep(60*60*1000); //60분
 		}
 		java.lang.Thread.sleep(10*1000); //1분
