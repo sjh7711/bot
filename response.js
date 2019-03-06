eb0939105124091c1bb3d90214354bf66d42b4f3
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-07 01:49:21 +0900

    Update response.js

--- response.js
+++ response.js
@@ -754,8 +754,7 @@ function sel(r){ //flag[2]==0&&flag[3]==0 -> 초기상태  // flag[2]==1&&flag[3
 				var temp = [];
 				if(Flag.get('sellist', r.room) == 0){
 					Flag.set("sellist", r.room, temp);
-				}
-				if(Flag.get('sellist', r.room) != 0){
+				} else if(Flag.get('sellist', r.room) != 0){
 					temp.concat(Flag.get('sellist', r.room));
 				}
 				
