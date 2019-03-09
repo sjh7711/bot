81669091ebbfda83f878d6dd2af6a7d3151e3b83
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-10 03:01:49 +0900

    Update response.js

--- response.js
+++ response.js
@@ -358,6 +358,7 @@ function baseball(r){
 		Flag.set('start1', r.room, 0);
 		r.replier.reply(Flag.get('baseball', r.room)[Flag.get('k', r.room)] + '님 차례입니다. 숫자만 입력해주세요.');
 		Flag.set('start2', r.room, 1);
+		return;
 	}
 	
 	if(Flag.get('start2', r.room) == 1) {
