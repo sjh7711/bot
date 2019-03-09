16af36833b47840316dc94e30acae36b68bb10b2
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-10 04:00:22 +0900

    Update response.js

--- response.js
+++ response.js
@@ -311,7 +311,7 @@ function func(r) {
 }
 
 function baseball(r){
-	if(D.selectForArray('baseball', 'name')[0].indexOf(r.sender) == -1){
+	if( D.selectForArray('baseball', 'name', 'room=?', r.room)[0] == undefined || D.selectForArray('baseball', 'name')[0].indexOf(r.sender) == -1){
 		D.insert('baseball', {name : r.sender, point : 10000, room : r.room});
 	}
 
