f5fcfdc01529cd372c621e9c18fbed26ef6dcd25
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-10 16:55:07 +0900

    Update response.js

--- response.js
+++ response.js
@@ -356,6 +356,10 @@ function baseball(r){
 				Flag.set('start', r.room, 0);
 				Flag.set('start1', r.room, 0);
 				Flag.set('start2', r.room, 0);
+				for(var i=0;i<Flag.get('baseball', r.room).length;i++){
+					var temppoint = Number(D.selectForArray('baseball', 'point', 'name=?', Flag.get('baseball', r.room)[i])[0])+1000;
+					D.update('baseball', {point : temppoint }, "name=?", Flag.get('baseball', r.room)[i]);
+				}
 				r.replier.reply('게임이 종료되었습니다. 새로운 게임이 가능합니다.');
 				return;
 			}
@@ -366,6 +370,10 @@ function baseball(r){
 		Flag.set('start', r.room, 0);
 		Flag.set('start1', r.room, 0);
 		Flag.set('start2', r.room, 0);
+		for(var i=0;i<Flag.get('baseball', r.room).length;i++){
+			var temppoint = Number(D.selectForArray('baseball', 'point', 'name=?', Flag.get('baseball', r.room)[i])[0])+1000;
+			D.update('baseball', {point : temppoint }, "name=?", Flag.get('baseball', r.room)[i]);
+		}
 		r.replier.reply('게임이 종료되었습니다. 새로운 게임이 가능합니다.');
 		return;
 	}
