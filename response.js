e00f3922ec2a6cca240a4fd59678e855d6ef062f
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-10 20:36:52 +0900

    Update response.js

--- response.js
+++ response.js
@@ -331,7 +331,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
     		saveImage(r);
     	}
         
-        if(room == '갠톡하기 귀찮아서 판 오버워치 카톡방'){
+        if(room == '갠톡하기 귀찮아서 판 오버워치 카톡방' || room == 'test'){
         	if(msg.indexOf('!뽑기')==0 && !isNaN(msg.substr(4))){
         		replier.reply(Math.Floor((Math.Random()*Number(msg.substr(4)))));
         	}
