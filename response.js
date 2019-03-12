5a4b5bba2efb2320a0cd7210496a4a64e1712adc
ApeachDev <ApeachDev@users.noreply.github.com>
2019-03-12 21:11:53 +0900

    Update response.js

--- response.js
+++ response.js
@@ -168,7 +168,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
         			replier.reply(sender+'님의 순위 : '+(Number(D.selectForArray('baseball',['name','point'], 'room=?', [room], {orderBy:"point desc"}).map(v=>v[0]).indexOf(r.sender))+1) + '등'
         			+'\n'+ sender+'님의 포인트 : '+D.selectForArray('baseball', 'point','name=? and room=?',[sender, room])
         			+'\n'+sender+'님의 야구 전적 : '+D.selectForArray('baseball', 'win','name=? and room=?',[sender, room])+'승 / '+D.selectForArray('baseball', 'lose','name=? and room=?',[sender, room])+'패'
-        			+'\n'+sender+'님의 야구 승률 : '+ (Math.floor((Number(D.selectForArray('baseball', 'win','name=? and room=?',[sender, room])) / (Number(D.selectForArray('baseball', 'lose','name=? and room=?',[sender, room]))+Number(D.selectForArray('baseball', 'win','name=? and room=?',[sender, room]))))*1000)/10)+'%';
+        			+'\n'+sender+'님의 야구 승률 : '+ Math.floor((Number(D.selectForArray('baseball', 'win','name=? and room=?',[sender, room])) / (Number(D.selectForArray('baseball', 'lose','name=? and room=?',[sender, room]))+Number(D.selectForArray('baseball', 'win','name=? and room=?',[sender, room]))))*1000)/10 + "%";
         			return;
         		}else {
         			replier.reply('알 수 없습니다.');
