d2376c68246b4a327b272c16cc40e8ef3d3f97eb
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-19 14:23:12 +0900

    Update response.js

--- response.js
+++ response.js
@@ -117,7 +117,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
         } 
         str += "!맛집\n"
         	
-        if (room == '시립대 전전컴 톡방' || room=='test' || room=='시립대 봇제작방') {
+        if (room == '시립대 전전컴 톡방' || room=='test' || room=='시립대 봇제작방' || room =='시립대 단톡방') {
             if (msg.indexOf("!최근채팅") == 0 || msg.indexOf("!ㅊㄱㅊㅌ") == 0) { recentchat(r); return;}
             str += "!최근채팅\n";
         }
