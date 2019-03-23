07b1118cd55f166480cc374a7975fc9efc1fea3d
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-23 19:29:01 +0900

    Update response.js

--- response.js
+++ response.js
@@ -373,7 +373,7 @@ function loadimage(r){
 		Flag.set('image', r.room, 1);
 	} else {
 		if(!isNaN(r.msg)){
-			r.replier.reply('ftp://sjh7711.iptime.org:2223/0/FTP/'+.substr(12));
+			r.replier.reply('ftp://sjh7711.iptime.org:2223/0/FTP/'+Flag.get('imagelist', r.room)[Number(r.msg)-1].substr(12));
 			Flag.set('image', r.room, 0);
 		}
 	}
