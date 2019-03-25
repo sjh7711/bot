adeef21a87fcd11e62c885e42586dcb1d722822e
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-03-25 21:41:42 +0900

    Update response.js

--- response.js
+++ response.js
@@ -781,6 +781,7 @@ function readFile() {
 }
 
 function checkstatus(r){
+	var reaction = ((new Date().getTime())-statustime)/1000;
 	var bm = Api.getContext().registerReceiver(null,new android.content.IntentFilter(android.content.Intent.ACTION_BATTERY_CHANGED));
 	var temperature = bm.getIntExtra("temperature",0)/10 + '℃'
 	var level = bm.getIntExtra("level",0) + "%"
@@ -797,7 +798,7 @@ function checkstatus(r){
 	var total = user+system+nice+idle;
 	var idlePerc = (1-idle/total)*100
 
-	batteryStatusStr = "배터리 상태\n"+"온도 : " + temperature +"\n충전률 : "+level + "\n상태 : " + status + "\n전압 : " + voltage + "\n기기 상태\n쓰레드 수 : "+T.getThreadList().length + "\nCPU : "+ Math.floor(idlePerc*100)/100 +"%\n"+"반응 속도 : "+((new Date().getTime())-statustime)/1000+'초';
+	batteryStatusStr = "배터리 상태\n"+"온도 : " + temperature +"\n충전률 : "+level + "\n상태 : " + status + "\n전압 : " + voltage + "\n기기 상태\n쓰레드 수 : "+T.getThreadList().length + "\nCPU : "+ Math.floor(idlePerc*100)/100 +"%\n"+"반응 속도 : "+reaction+'초';
 	r.replier.reply(batteryStatusStr);
 }
 
