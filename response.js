1fc840e71794bb8a47a99d73a93568cdc9e22484
sjh7711 <46289909+sjh7711@users.noreply.github.com>
2019-04-25 12:56:17 +0900

    Update response.js

--- response.js
+++ response.js
@@ -380,7 +380,7 @@ function func(r) {
 }
 
 function calculator(r){
-	r.msg = r.msg.substr(4).replace(/[가-힣ㄱ-ㅎㅏ-ㅣA-Za-z]/g, "");
+	r.msg = r.msg.substr(4).replace(/[^0-9*\-+%/*=\^&|!]/g, "");
 	r.replier.reply(eval(r.msg));
 }
 
