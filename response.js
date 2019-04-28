58e1416856295f63b4082ac6ef838a4a6f74b357
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-04-29 01:59:44 +0900

    Update response.js

--- response.js
+++ response.js
@@ -596,7 +596,7 @@ function blackjack(r){
         }
     }
 	
-	if ( gameinfo.start == 1 && (gameinfo.playerlist.length == 3 || (r.msg == '시작' && gameinfo.playerlist.indexOf(r.sender) > -1 )) ){
+	if ( gameinfo.start == 0 && (gameinfo.playerlist.length == 3 || (r.msg == '시작' && gameinfo.playerlist.indexOf(r.sender) > -1 )) ){
 		var figure = ['♣','♠','♦','♥'];
 		var num = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
 		var temp = [];
