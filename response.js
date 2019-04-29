0226540b2e6f6902f41483574807051af2a0f63b
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-04-29 21:21:53 +0900

    Update response.js

--- response.js
+++ response.js
@@ -699,7 +699,7 @@ function blackjack(r){
 		}
 	}
 	
-	if ( gameinfo.start3 == 1 && !isNaN(r.msg) && gameinfo.insurlist.indexOf(r.sender) == -1 && gameinfo.insurlist.length < gameinfo.playerlist.length &&  Number(gameinfo['player'+i].bet/2) >= r.msg && r.msg > -1  ){
+	if ( gameinfo.start3 == 1 && !isNaN(r.msg) && gameinfo.insurlist.indexOf(r.sender) == -1 && gameinfo.insurlist.length < gameinfo.playerlist.length &&  Number(gameinfo['player'+num].bet/2) >= r.msg && r.msg > -1  ){
 		gameinfo.insurlist.push(r.sender);
 		gameinfo['player'+num].insurance = r.msg;
 		if(gameinfo.insurlist.length == gameinfo.playerlist.length){
