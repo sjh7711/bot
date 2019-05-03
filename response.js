d42182eb84ed19272685535748ba344a463de722
sjh7711 <46289909+sjh7711@users.noreply.github.com>
2019-05-03 13:53:59 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1273,12 +1273,14 @@ function inform(r){
 	if(D.selectForArray('baseball',null,'name=? and room=?',[r.sender, r.room])!=undefined){
 		var wincount = Number(D.selectForArray('baseball', 'win','name=? and room=?',[r.sender, r.room]));
 		var losecount = Number(D.selectForArray('baseball', 'lose','name=? and room=?',[r.sender, r.room]));
-		r.replier.reply(r.sender+'님의 정보'
-		+'\n순위 : '+(Number(D.selectForArray('baseball',['name','point'], 'room=?', [r.room], {orderBy:"point desc"}).map(v=>v[0]).indexOf(r.sender))+1) + '등'
-		+'\n포인트 : '+D.selectForArray('baseball', 'point','name=? and room=?',[r.sender, r.room])
-		+'\n전적 : '+wincount+'승 / '+losecount+'패'
-		+'\n승률 : '+ Math.floor( wincount / (losecount + wincount)*1000)/10 + "%")
-		+'\n초기화카운트 : '+ Number(2 - D.selectForArray('baseball', 'clear', 'room=? and name = ?', [room, sender])));
+		var str = '';
+		str += r.sender+'님의 정보';
+		str += '\n순위 : '+(Number(D.selectForArray('baseball',['name','point'], 'room=?', [r.room], {orderBy:"point desc"}).map(v=>v[0]).indexOf(r.sender))+1) + '등';
+		str += '\n포인트 : '+D.selectForArray('baseball', 'point','name=? and room=?',[r.sender, r.room]);
+		str += '\n전적 : '+wincount+'승 / '+losecount+'패';
+		str += '\n승률 : '+ Math.floor( wincount / (losecount + wincount)*1000)/10 + "%");
+		str += '\n초기화카운트 : '+ Number(2 - D.selectForArray('baseball', 'clear', 'room=? and name = ?', [room, sender]));
+		r.replier.reply(str);
 		return;
 	}else {
 		r.replier.reply('알 수 없습니다.');
