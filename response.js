123178fa60f905959c3f6826fb5cb9aa4ae568ad
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-05 12:07:12 +0900

    Update response.js

--- response.js
+++ response.js
@@ -4,6 +4,7 @@ if(ObjKeep.get("reboottime")==null){
 }
 var reloadtime = new Date().getTime();
 var D = require("DBManager.js")("D");
+var K = require("KBManager.js")("/data/data/com.kakao.talk/databases/KakaoTalk2.db");
 var T = require("ThreadManager.js");
 var I = require("Interactive.js");
 var control = D.selectForArray('control').map(v=>v[0]);
