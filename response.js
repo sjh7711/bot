dc980d44abe7bd9bbc635fc5fb3fa08562a543aa
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-05 14:51:39 +0900

    Update response.js

--- response.js
+++ response.js
@@ -2647,13 +2647,13 @@ function time() {
 	ampm   = hour >= 12 ? 'PM' : 'AM';
 	
 	hour1 = hour % 12;
-	hour1 = hour ? hour : 12;
+	hour1 = hour1 < 10 ? '0' + hour1 : hour1;
 	
 	hour = hour < 10 ? '0' + hour : hour;
 	minute = minute < 10 ? '0' + minute : minute;
 	second = second < 10 ? '0' + second : second;
 	
-	var now = year + '년 ' + month + '월 ' + date + '일 ' + day + ' ' + hour + ':' + minute + ':' + second + ' ' + ampm;
+	var now = year + '년 ' + month + '월 ' + date + '일 ' + day + ' ' + hour1 + ':' + minute + ':' + second + ' ' + ampm;
 	
 	return { now : now , year : year, month : month , date : date, day : day, hour : hour , minute : minute , second : second, ampm : ampm , hour1: hour1};
 }
