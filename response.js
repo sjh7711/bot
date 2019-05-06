d323fdac6792e411704c43b2cf994169ac2744d3
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-06 18:01:17 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1587,13 +1587,13 @@ function weather(r){
 		        	if(!isNaN(msg) && msg>=1 && msg<=name.length){
 		        		var targetNum=msg-1;
 		        		link2 = org.jsoup.Jsoup.connect(link1.select('div.lcl_lst').select('a').get(targetNum).attr("abs:href")).get().select('div.api_more_wrap').select('a').attr("abs:href");
-		        		check = String(link2).indexOf('weather');
+		        		check = link2.indexOf('weather');
 		        		where = name[targetNum].substr(3) ;
 		        	}
 		        }
 			}
 			
-			if(String(link2).indexOf('regionCode')==-1){
+			if(link2.indexOf('regionCode')==-1){
 				r.replier.reply("검색이 불가능합니다.");
         		return;
 			}
