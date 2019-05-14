134cf718df86e725a70d915c28b5e973bad0e675
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-15 01:04:27 +0900

    Update response.js

--- response.js
+++ response.js
@@ -988,7 +988,7 @@ function blackjack(r){
 						var temp = gameinfo.splitdata.filter(v=>v.name == r.sender);
 						for(var j in temp ) {
 							var temppoint1 = D.selectForArray('blackjack', 'point', 'name=? and room=?', [temp[j].name, r.room])[0][0];
-							/*if(temp[j].state == 1){
+							if(temp[j].state == 1){
 								str += temp[j].name+'님 ('+temp[j].sum+') : Lose\n⤷[' + temp[j].card.map(v=>v.join(' ')).join(' | ')+']\n';
 								var temppoint = temppoint1-Number(temp[j].bet);
 							} else if (temp[j].state == 5){
@@ -1003,7 +1003,7 @@ function blackjack(r){
 							} else if ( (temp[j].state == 6 && gameinfo.dealer.sum > temp[j].sum ) || temp[j].state == 7){
 								str += temp[j].name+'님 ('+temp[j].sum+') : DoubleDownLose\n⤷[' + temp[j].card.map(v=>v.join(' ')).join(' | ')+']\n';
 								var temppoint = temppoint1-Number(temp[j].bet*2);
-							} else if (temp[j].state == 4){
+							} /*else if (temp[j].state == 4){
 								str += temp[j].name+'님 ('+temp[j].sum+') : Blackjack\n⤷[' + temp[j].card.map(v=>v.join(' ')).join(' | ')+']\n';
 								var temppoint = temppoint1+Number(temp[j].bet*1.5);
 							} else if( gameinfo.dealer.sum < temp[j].sum ){
