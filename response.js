e2e96fd742081ff354d12bb6150821ae6a9e34c5
sjh7711 <46289909+sjh7711@users.noreply.github.com>
2019-05-15 12:29:32 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1478,7 +1478,7 @@ function inform(r){
 		var losecount = D.selectForArray('baseball', 'lose','name=? and room=?',[r.sender, r.room])[0][0];
 		var str = '';
 		str += r.sender+'님의 정보';
-		str += '\n순위 : '+Number(D.selectForArray('baseball',['name','point'], 'room=?', [r.room], {orderBy:"point desc"}).map(v=>v[0]).indexOf(r.sender))+1 + '등';
+		str += '\n순위 : '+Number(D.selectForArray('baseball',['name','point'], 'room=?', [r.room], {orderBy:"point desc"}).map(v=>v[0]).indexOf(r.sender)+1) + '등';
 		str += '\n포인트 : '+D.selectForArray('baseball', 'point','name=? and room=?',[r.sender, r.room])[0][0];
 		str += '\n전적 : '+wincount+'승 / '+losecount+'패';
 		str += '\n승률 : '+ Math.floor( wincount / (losecount + wincount)*1000)/10 + "%";
