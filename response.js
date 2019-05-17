54d09915c1facb97896d4dda130ca453df4dfe29
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-17 23:36:04 +0900

    Update response.js

--- response.js
+++ response.js
@@ -154,6 +154,11 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
 			return;
 		}
 		
+		if (msg.indexOf("!함수로딩") == 0 && work == 1){
+			functionreload(r);
+			return;
+		} 
+		
 		if (msg == '!리부트' && work == 1){
 			replier.reply('Rebooting...');
 			cmd('reboot');
@@ -457,6 +462,34 @@ function funcCheck(r){
 	str1 += '\n';
 	str1 = str1.split(' / \n').join('\n');
 	return str1;
+}
+
+function functionreload(r){
+	if(Flag.get('freloadcheck', r.room)==0){
+		Flag.set('function', r.room, File("/sdcard/kbot/functions").listFiles());
+		var temp = [];
+		for(var i in Flag.get('function', r.room)){
+			if(String(Flag.get('function', r.room)[i]).indexOf(r.msg.substr(6))>-1) {
+				temp.push(Flag.get('function', r.room)[i]);
+			}
+		}
+		if(temp.length == 0){
+			r.replier.reply('검색결과가 없습니다.');
+			return;
+		}
+		Flag.set('function', r.room, temp);
+		var i = 1;
+		r.replier.reply('함수 개수 : '+Flag.get('function', r.room).length+'\n번호를 선택하세요.\n'+Flag.get('function', r.room).map(v=> (i++)+'. ' + String(v).substr(23)).join('\n'));
+		Flag.set('freloadcheck', r.room, 1);
+	} else if ( Flag.get('freloadcheck', r.room)== 1){
+		if(!isNaN(r.msg)){
+			eval( Flag.get('imagelist', r.room)[Number(r.msg)-1] );
+			r.replier.reply(Flag.get('imagelist', r.room)[Number(r.msg)-1] + '리로딩 완료');
+		} else {
+			r.replier.reply('숫자를 입력하세요.');
+			Flag.set('freloadcheck', r.room, 0);
+		}
+	}
 }
 
 function saveImage(r) {
