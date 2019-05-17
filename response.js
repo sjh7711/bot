645a20645a1c2f8bbf4669f5856d2c0a03705673
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-17 23:46:11 +0900

    Update response.js

--- response.js
+++ response.js
@@ -507,6 +507,167 @@ function saveImage(r) {
     }
 }
 
+function time() {
+	var today = new Date();
+	var dayNames = ['(일요일)', '(월요일)', '(화요일)', '(수요일)', '(목요일)', '(금요일)', '(토요일)'];
+	var day = dayNames[today.getDay()];
+	
+	var year   = today.getFullYear(),
+	month  = today.getMonth() + 1,
+	date   = today.getDate(),
+	hour   = today.getHours(),
+	minute = today.getMinutes(),
+	second = today.getSeconds();
+	ampm   = hour >= 12 ? 'PM' : 'AM';
+	
+	hour1 = hour % 12;
+	hour1 = hour1 < 10 ? '0' + hour1 : hour1;
+	
+	hour = hour < 10 ? '0' + hour : hour;
+	minute = minute < 10 ? '0' + minute : minute;
+	second = second < 10 ? '0' + second : second;
+	
+	var now = year + '년 ' + month + '월 ' + date + '일 ' + day + ' ' + hour1 + ':' + minute + ':' + second + ' ' + ampm;
+	
+	return { now : now , year : year, month : month , date : date, day : day, hour : hour , minute : minute , second : second, ampm : ampm , hour1: hour1};
+}
+
+cloneObject = function (obj) {
+	  return JSON.parse(JSON.stringify(obj));
+	}
+
+thread = function (r){
+	r.replier.reply(T.getThreadList().join('\n'));
+}
+
+db = function (r){
+	r.replier.reply(D.selectForString("sqlite_master"));
+}
+
+checkroom = function (r){
+	r.replier.reply(Api.getRoomList().slice().join('\n'));
+}
+
+suggestion = function (r){
+	if(r.msg.length < 7 ){
+		r.replier.reply("건의가 너무 짧습니다.");
+	}else{
+		Api.replyRoom('test', r.room+" : "+r.sender+" : "+r.msg.substr(4));
+		r.replier.reply(r.sender+"님의 건의가 접수되었습니다.");
+	}
+}
+
+isread = function (is) {
+  var br = new java.io.BufferedReader(new java.io.InputStreamReader(is));
+  var readStr = "";
+  var str = null;
+  while (((str = br.readLine()) != null)) {
+      readStr += str + "\n";
+  }
+  br.close();
+  return readStr.trim();
+}
+
+cmd = function (dir){
+	var p = java.lang.Runtime.getRuntime().exec('su -c ""'+dir+'""');
+  p.waitFor();
+  var r = p.getInputStream() || p.getErrorStream();
+  return isread(r);
+}
+
+compare = function (a, b) {
+  return a - b;
+}
+
+File = java.io.File;
+
+own = function(obj){
+	return Object.getOwnPropertyNames(obj);
+	}
+
+String.prototype.replaceAmp=function(){
+	var res=this.toString();
+	var tmp;
+	while(tmp=/&#x....;/.exec(res)){
+		res=res.replace(tmp[0],String.fromCharCode(parseInt(tmp[0].substr(3,4),16)));
+	}
+	while(tmp=/&#..;/.exec(res)){
+		res=res.replace(tmp[0],String.fromCharCode(parseInt(tmp[0].substr(2,2))));
+	}
+	return res.replace(/&nbsp;/g,"\t").replace(/&gt;/g,">").replace(/&lt;/g,"<").replace(/&quot;/g,'"').replace(/&amp;/g,"&");
+}
+String.prototype.replaceAmp2=function(){
+	var res=this.toString();
+	var tmp;
+	while(tmp=/&#x....;/.exec(res)){
+		res=res.replace(tmp[0],String.fromCharCode(parseInt(tmp[0].substr(3,4),16)));
+	}
+	return res.replace(/&nbsp;/g,"\t").replace(/&gt;/g,">").replace(/&lt;/g,"<").replace(/&quot;/g,'"').replace(/&amp;/g,"&").replace(/<br>/g,"\n");
+}
+String.prototype.indexOfs=function(){  
+	var res = -1;
+	for (var i = 0; i < arguments.length; i++) {
+		var tmp=this.toString().indexOf(arguments[i]);
+		if(tmp==-1) continue;
+		else if(res==-1) res=tmp;
+		else if(tmp<res) res=this.toString().indexOf(arguments[i]);
+	}
+	return res;
+}
+String.prototype.includess=function(){
+	for (var i = 0; i < arguments.length; i++) {
+		if(this.toString().includes(arguments[i])) return true;
+	}
+	return false;
+}
+String.prototype.replaces=function(target){
+	var res=this.toString();
+	for (var i = 1; i < arguments.length; i++) {
+		res=res.replace(arguments[i],target);
+	}
+	return res;
+}
+String.prototype.encoding=function(){
+ return this.replace(/\\u([\da-fA-F]{4})/g,(m,p1)=>String.fromCharCode(parseInt(p1,16)));
+}
+
+String.prototype.받침=function(){
+	var lastCharCode=this.toString().charCodeAt(this.toString().length-1);
+	if(lastCharCode>="가".charCodeAt(0) && lastCharCode<="힣".charCodeAt(0)){
+		if((lastCharCode-"가".charCodeAt(0))%28==0) return false;
+		else return true;
+	}else return false;
+	
+}
+String.prototype.은는=function(){
+	return this.toString().받침() ? this.toString()+"은" : this.toString()+"는"; 
+}
+String.prototype.이가=function(){
+	return this.toString().받침() ? this.toString()+"이" : this.toString()+"가"; 
+}
+String.prototype.과와=function(){
+	return this.toString().받침() ? this.toString()+"과" : this.toString()+"와"; 
+}
+String.prototype.을를=function(){
+	return this.toString().받침() ? this.toString()+"을" : this.toString()+"를"; 
+}
+String.prototype.조사=function(받침있음, 받침없음){
+	return this.toString().받침() ? this.toString()+받침있음 : this.toString()+받침없음;
+}
+
+String.format=function(str,arg){
+	if(str.charAt(str.length-1).toLowerCase()=='d') return String(new java.lang.String.format(str,new java.lang.Integer(arg)));
+	return String(new java.lang.String.format(str,arg));	
+}
+String.prototype.extension=function(char,length){
+	const addLength = (length-this.toString().length >= 0) ? length-this.toString().length : 0; 
+	return char.repeat(addLength)+this.toString();
+}
+String.prototype.extensionRight=function(char,length){
+	const addLength = (length-this.toString().length >= 0) ? length-this.toString().length : 0; 
+	return this.toString()+char.repeat(addLength);
+}
+
 /*
 function calculator(r){
 	var temp = eval(r.msg.substr(1).replace(/[^0-9*\-+%/*=\^&|!.~{}()[\]]/g, ""));
