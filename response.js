3b77fec4f48afa312cd2570fa8529a8313eb6b2a
ApeachDev <46289909+sjh7711@users.noreply.github.com>
2019-05-17 22:26:31 +0900

    Update response.js

--- response.js
+++ response.js
@@ -55,11 +55,7 @@ function reload(r) {
 	    control = D.selectForArray('control').map(v=>v[0]);
 	    controlPanel = D.selectForObject('control');
 	    Api.replyRoom(r.room , "Response reloading 완료 / " + ((new Date() - Timer) / 1000) + "s\n" + new Date() );
-	    eval(
-	    	for( var i in File("/sdcard/kbot/functions").listFiles()) {
-	    		eval( readFile(File("/sdcard/kbot/functions").listFiles()[i]));
-	    		}
-	    	);
+	    eval(var functions = File("/sdcard/kbot/functions").listFiles();for(var i in functions){eval( readFile(functions[i]) )} )
 	    Api.replyRoom(r.room , "Function reloading 완료 / " + ((new Date() - Timer) / 1000) + "s\n" + new Date());
 	}
 }
