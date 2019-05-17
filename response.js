cd7efc5ccc13d31f87bbf10e04390d842d0ad0b1
sjh7711 <46289909+sjh7711@users.noreply.github.com>
2019-05-17 11:34:29 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1030,7 +1030,7 @@ function blackjack(r){
 	
 	if( gameinfo.splitdata.filter(v=>v.name == r.sender).filter(v=>v.end == 0).length > 0 && gameinfo['player'+num].state > 0 ){
 		while(1){
-			if(gameinfo['player'+num].end == 1 && gameinfo.splitdata.filter(v=>v.name == r.sender).filter(v=>v.end == 0)[0].end == 0){
+			if(gameinfo['player'+num].end == 1 && gameinfo.splitdata.filter(v=>v.name == r.sender).filter(v=>v.end == 0).length > 0 ){
 				gameinfo.splitdata.push( cloneObject(gameinfo['player'+num]) );
 				gameinfo['player'+num]= null;
 				gameinfo['player'+num]= cloneObject(gameinfo.splitdata.filter(v=>v.name == r.sender).filter(v=>v.end == 0)[0]);
