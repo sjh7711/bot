6b45ff513836af3540ee8ee920e753e3f9560e45
sjh7711 <46289909+sjh7711@users.noreply.github.com>
2019-05-17 09:32:46 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1003,7 +1003,7 @@ function blackjack(r){
 						if(gameinfo['player'+num].sum == 21){
 							gameinfo['player'+num].state = 2;
 							gameinfo.endcount +=1;
-							gameinfo.end = 1;
+							gameinfo['player'+num].end = 1;
 							str += '\n'+gameinfo['player'+num].name + '님의 Stay!'+'  ('+gameinfo.endcount+'/'+(gameinfo.playerlist.length+gameinfo.splitdata.length)+')';
 							r.replier.reply(str);
 						} else {
@@ -1050,7 +1050,7 @@ function blackjack(r){
 				if(gameinfo['player'+num].sum == 21){
 					gameinfo['player'+num].state = 2;
 					gameinfo.endcount +=1;
-					gameinfo.end = 1;
+					gameinfo['player'+num].end = 1;
 					str += '\n'+gameinfo['player'+num].name + '님의 Stay!';
 					str += '  ('+gameinfo.endcount+'/'+(gameinfo.playerlist.length+gameinfo.splitdata.length)+')'
 					r.replier.reply(str);
